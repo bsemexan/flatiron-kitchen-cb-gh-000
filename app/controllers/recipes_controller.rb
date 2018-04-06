6e29e232d26733137d43d20c8589be8cebe2dc7b
Berry  Semexan <bsemexan@gmail.com>
2018-04-06 20:48:44 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,4 +13,9 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  private 
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
 end

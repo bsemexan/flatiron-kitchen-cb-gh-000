b0f1c7fcf0f86e14f834131abe6a6d8dd2ebad78
Berry  Semexan <bsemexan@gmail.com>
2018-04-06 21:56:28 +0000

    Done.

---
+++ db/migrate/20180406211808_create_recipe_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateRecipeIngredients < ActiveRecord::Migration
+  def change
+    create_table :recipe_ingredients do |t|
+      t.integer :recipe_id
+      t.integer :ingredient_id
+
+      t.timestamps null: false
+      add_foreign_key :recipe_ingredients, :recipes
+      add_foreign_key :recipe_ingredients, :ingredients
+    end
+  end
+end

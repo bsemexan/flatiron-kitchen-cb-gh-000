5e6313f10f16a1a3001afcb87c19914985153d3e
Berry  Semexan <bsemexan@gmail.com>
2018-04-06 20:49:22 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to ingredients_path
+      redirect_to recipes_path
     else
       render :new
     end
